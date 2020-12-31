2d428070f15a53faa1f56d3db4ddc50db38ba32a
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2020-12-31 16:34:29 +0100

    Add files via upload

---
+++ src/MainClass.java
@@ -0,0 +1,22 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * Author: Richard Meoli
+		 * 
+		 * Class: MainClass (main method)
+		 * Lines: 22
+		 * Description: this class instances an object of the class HomeGui, which shows up the mainframe.
+		 * 
+		 * Total Lines: 3021
+		 * 
+		 */
+		
+		new HomeGui("My Books");
+
+	}
+
+}
